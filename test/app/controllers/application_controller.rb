e587acc5422d7327c0a192e3110bad95acb1e6bf
Jake <github@jake.otherinbox.com>
2010-10-12 11:39:19 -0700

    Cleanup

--- test/app/controllers/application_controller.rb
+++ test/app/controllers/application_controller.rb
@@ -1,13 +1,4 @@
 class ApplicationController < ActionController::Base
-#
-#	def redirections
-#		@redirections ||= HashWithIndifferentAccess.new({
-#			:not_be_user => {
-#				:redirect_to => user_path(current_user)
-#			}
-#		})
-#	end
-#
 
 	helper :all # include all helpers, all the time
 
