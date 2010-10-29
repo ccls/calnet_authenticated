56ddd598fefca67c399408d0804f11d34b0ea2e5
Jake <github@jake.otherinbox.com>
2010-10-29 16:24:53 -0700

    Testing gems using gem code in place.

--- generators/calnet_authenticated/calnet_authenticated_generator.rb
+++ generators/calnet_authenticated/calnet_authenticated_generator.rb
@@ -28,17 +28,57 @@ require 'calnet_authenticated/autotest'
 
 			m.migration_template 'migration.rb', 'db/migrate', 
 				:migration_file_name => "add_calnet_authenticated_columns_to_#{file_path.gsub(/\//, '_').pluralize}"
+
+			m.directory('public/javascripts')
+			Dir["#{File.dirname(__FILE__)}/templates/javascripts/*js"].each{|file| 
+				f = file.split('/').slice(-2,2).join('/')
+				m.file(f, "public/javascripts/#{File.basename(file)}")
+			}
+			m.directory('public/stylesheets')
+			Dir["#{File.dirname(__FILE__)}/templates/stylesheets/*css"].each{|file| 
+				f = file.split('/').slice(-2,2).join('/')
+				m.file(f, "public/stylesheets/#{File.basename(file)}")
+			}
+#			m.directory('test/functional/calnet_authenticated')
+#			Dir["#{File.dirname(__FILE__)}/templates/functional/*rb"].each{|file| 
+#				f = file.split('/').slice(-2,2).join('/')
+#				m.file(f, "test/functional/calnet_authenticated/#{File.basename(file)}")
+#			}
+#			m.directory('test/unit/calnet_authenticated')
+#			Dir["#{File.dirname(__FILE__)}/templates/unit/*rb"].each{|file| 
+#				f = file.split('/').slice(-2,2).join('/')
+#				m.file(f, "test/unit/calnet_authenticated/#{File.basename(file)}")
+#			}
 		end
 	end
 
-#	I have no idea what I'm doing.  Monkey see, monkey do.
-#	This does seem to work though.
-#	class_name = the first argument passed to the generator
-#	file_path  = class_name.underscore
-#		tableize would have been good
-#	AddTrackingNumberTo
-#				:assigns => { :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}" }, 
-#	
-#	where does "file_path" come from?
-
+end
+module Rails::Generator::Commands
+	class Create
+		def migration_template(relative_source, 
+				relative_destination, template_options = {})
+			migration_directory relative_destination
+			migration_file_name = template_options[
+				:migration_file_name] || file_name
+			if migration_exists?(migration_file_name)
+				puts "Another migration is already named #{migration_file_name}: #{existing_migrations(migration_file_name).first}: Skipping" 
+			else
+				template(relative_source, "#{relative_destination}/#{next_migration_string}_#{migration_file_name}.rb", template_options)
+			end
+		end
+	end #	Create
+	class Base
+	protected
+		#	the loop through migrations happens so fast
+		#	that they all have the same timestamp which
+		#	won't work when you actually try to migrate.
+		#	All the timestamps MUST be unique.
+		def next_migration_string(padding = 3)
+			@s = (!@s.nil?)? @s.to_i + 1 : if ActiveRecord::Base.timestamped_migrations
+				Time.now.utc.strftime("%Y%m%d%H%M%S")
+			else
+				"%.#{padding}d" % next_migration_number
+			end
+		end
+	end	#	Base
 end
